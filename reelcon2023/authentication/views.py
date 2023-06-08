ce51a45c4d8d2aae67c014ed4550e793012012c5
sdrtharun <mailtharunsdr@gmail.com>
2023-06-08 11:06:35 +0530

    configure urls

---
+++ reelcon2023/authentication/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(response):
+    return HttpResponse("<h1>Lets goo its just the begining</h1>")
